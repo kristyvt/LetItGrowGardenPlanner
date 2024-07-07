4a71d8e8e9a6bf85ff2f82ff1c9b64a043397235
Kristy Stark <kristystarkvt@gmail.com>
2024-07-07 12:50:13 -0400

    Initial Commit

---
+++ export_query.py
@@ -0,0 +1,31 @@
+import csv
+import data_connection
+import os
+
+outcome_detail_query = 'QueryOutcomeDetail'
+
+class ExportQuery:
+    def __init__(self):
+        self.header = []
+
+    def export_csv(self, query_name, header):
+
+        this_connection = data_connection.Connection()
+        cursor = this_connection.connection.cursor()
+
+        download_path = os.path.expanduser("~") + '\\Downloads\\' + query_name + '.csv'
+        print(download_path)
+
+        cursor.execute(query_name)
+
+        data = cursor.fetchall()
+
+        with open(download_path, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(header)
+            for row in data:
+                writer.writerow(row)
+                print(row)
+
+
+        return download_path
