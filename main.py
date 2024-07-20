80bcb87cf5171bc008a261cb43d405ac7d10c824
Kristy Stark <kristystarkvt@gmail.com>
2024-07-20 15:35:38 -0400

    added check for database connection before launching window

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-
-import window
-import plant
+# Import data connection to start program
 import data_connection
-import plant_set
 
-run = window.Window()
+# Test that database exists and can connect before launching
+
+test_connection = data_connection.Connection()
+if test_connection.status == "success":
+    import window
+    import plant
+    import plant_set
+elif test_connection.status == "failed":
+    print("Database does not exist, please install Garden database and try again")
+else:
+    print("other error")
 
 
 exit(0)
