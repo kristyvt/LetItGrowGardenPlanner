51bce9db93b4ff85c9b578865be572b6c37c8a90
Kristy Stark <kristystarkvt@gmail.com>
2024-07-20 15:34:00 -0400

    added error handling for failed connection

--- data_connection.py
+++ data_connection.py
@@ -8,15 +8,41 @@ class Connection:
     def __init__(self):
         server = 'KRISTY-LAPTOP\\SQLEXPRESS'
         database = 'Garden'
+        self.status = 'pending'
 
-        connection_string = \
-            (f'DRIVER={{ODBC Driver 18 for SQL Server}};'
-             f'SERVER={server};'
-             f'DATABASE={database};'
-             f'trusted_connection=yes;'
-             f'Encrypt=No')
+        try:
 
-        self.connection = pyodbc.connect(connection_string)
+            connection_string = \
+                (f'DRIVER={{ODBC Driver 18 for SQL Server}};'
+                 f'SERVER={server};'
+                 f'DATABASE={database};'
+                 f'trusted_connection=yes;'
+                 f'Encrypt=No')
+
+            self.connection = pyodbc.connect(connection_string)
+            self.status = "success"
+
+        except:
+            print("no connection available")
+            self.status = "failed"
 
     def end_connection(self):
         self.connection.close()
+
+
+    def connect_to_master(self):
+        try:
+
+            connection_string = \
+                (f'DRIVER={{ODBC Driver 18 for SQL Server}};'
+                 f'SERVER={'KRISTY-LAPTOP\\SQLEXPRESS'};'
+                 f'DATABASE={'master'};'
+                 f'trusted_connection=yes;'
+                 f'Encrypt=No')
+
+            self.connection = pyodbc.connect(connection_string)
+            self.status = "success"
+
+        except:
+            print("no connection to master")
+            self.status = "failed"
